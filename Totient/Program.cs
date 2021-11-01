c6d901df686230178018663d813882f44d11e007
umtas <umtas@DESKTOP-QQMM5IC>
2021-11-02 01:29:35 +0300

    Add project files.

---
+++ Totient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Totient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Totient(12));
+           
+        }
+
+        static int Totient(int n)
+        {
+            int count = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (obeb(n, i) == 1)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static int obeb(int x, int y)
+        {
+            int z = 0;
+
+            for (int i = 1; i <= Math.Max(x, y); i++)
+            {
+                if (x % i == 0 && y % i == 0)
+                {
+                    z = i;
+                }
+            }
+
+            return z;
+        }
+    }
+        
+}
+
+
+    
+
